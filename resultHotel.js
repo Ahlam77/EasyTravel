96169fd2e2cb292e5156b151ffd64383678a317c
Grace Kabeya <antoinefaith1@gmail.com>
2021-04-03 17:10:02 +0200

    commit

--- resultHotel.js
+++ resultHotel.js
@@ -12,6 +12,7 @@ var hotelId = '';
 var title = document.getElementsByTagName('h1')[1];
 var divElement = document.getElementsByClassName('info')[0];
 var divRooms = document.createElement('div');
+var container = document.getElementById('Iflex');
 
 if(urlParams.has('hotelId')){
 	hotelId = urlParams.get('hotelId');
@@ -52,7 +53,7 @@ if(hotelId === ''){
 					container: 'map-result',
 					style: 'mapbox://styles/mapbox/streets-v11',
 					center: loc1, // starting position
-					zoom: 9 // starting zoom
+					zoom: 15 // starting zoom
 				});
 				map.addControl(new mapboxgl.NavigationControl());
 				// Set options
@@ -67,8 +68,76 @@ if(hotelId === ''){
 				var h3 = document.getElementById('h3')
 				h3.innerHTML = result.data.hotel.name
 				h3.style.fontFamily = "roboto"
-				/*result.data.offers.forEach(element => {
-					var divTitle = document.createElement('div');
+				h3.style.fontWeight = 'bold'
+				result.data.offers.forEach(element => {
+					/*var text = document.getElementById('info-room')
+					text.style.fontSize = '20px'
+					text.style.fontWeight = 'bold'
+					var text1 = document.getElementById('info-room1')
+					text1.style.fontSize = '20px'
+					text1.style.fontWeight = 'bold'
+					var text2 = document.getElementById('info-room2')
+					text2.style.fontSize = '20px'
+					text2.style.fontWeight = 'bold'
+					
+					text.innerHTML ="Suit-type:"+" "+ element.room.typeEstimated.bedType
+					text1.innerHTML = "Rooms:"+" "+ element.room.typeEstimated.beds
+					text2.innerHTML = "Category:"+" "+ element.room.typeEstimated.category*/
+					var infoRooms = document.getElementById('info-rooms')
+					var divElement = document.createElement('div')
+					var divImage = document.createElement('div')
+					var divPara = document.createElement('div')
+					divElement.appendChild(divImage)
+					var image = document.createElement('img')
+					image.setAttribute('src', './Image/place-10.jpg')
+
+					image.style.width = '100px'
+					image.style.height = '90px'
+					
+					divImage.appendChild(image)
+					divElement.appendChild(divPara)
+					
+					var ptag = document.createElement('p')
+					var ptag2 = document.createElement('p')
+					var ptag3 = document.createElement('p')
+					var button = document.createElement('button')
+
+
+					ptag.innerHTML = 'Price:'+" "+element.price.total+""+""+'$'
+					ptag2.innerHTML = 'Category:'+' '+element.room.typeEstimated.category
+					button.innerText = 'Book Now'
+					
+					//
+
+					divPara.appendChild(ptag)
+					divPara.appendChild(ptag2)
+					divPara.appendChild(button)
+					divElement.setAttribute('id', 'info-rooms')
+					divElement.style.width = '600px'
+					divElement.style.height = '120px'
+					
+					divElement.style.color = 'grey'
+					divElement.style.border = '3px solid black'
+					divElement.style.borderRadius = '10px'
+					divElement.style.display = 'flex'
+					divElement.style.flexDirection = 'row'
+					divPara.style.display = 'block'
+					divPara.style.margin = '0 15px'
+					button.style.backgroundColor = 'blue'
+					button.style.border = 'none'
+					button.style.color = 'white'
+					button.style.borderRadius = '10px'
+
+					container.appendChild(divElement)
+				
+					
+					
+
+
+
+
+
+					/*var divTitle = document.createElement('div');
 					var divContent = document.createElement('div');
 					divTitle.innerHTML = element.room.typeEstimated.category;
 					divTitle.style.backgroundColor = 'rgb(90, 90, 247)';
@@ -132,8 +201,8 @@ if(hotelId === ''){
 					divContent.appendChild(divCart);
                     divRooms.setAttribute('class', 'containaer-card');
 					divRooms.appendChild(divContent);
-					divElement.appendChild(divRooms);
-				});*/
+					divElement.appendChild(divRooms);*/
+				});
 				console.log(result);
 			})
             .catch(error => console.log('error', error));
