d51f6a8c22ddcb98cb2072560836dbdbd433259f
ahlam alshaba <ama1977ln@outlook.com>
2021-04-05 21:08:31 +0200

    adding

--- appartment.js
+++ appartment.js
@@ -1,6 +1,6 @@
-function addAppartement(){
-    const user ={
-        "firstName": document.getElementById('first-name').value, 
+function addAppartement() {
+    const user = {
+        "firstName": document.getElementById('first-name').value,
         "lastName": document.getElementById('last-name').value,
         "phoneNumber": document.getElementById('tel').value,
         "countryName": document.getElementById('country').value,
@@ -12,24 +12,26 @@ function addAppartement(){
 
     var json = JSON.stringify(user);
 
-    fetch('http://localhost:3000/users',{
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: json
-    })
-    .then(data => console.log(data));
+    fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: json
+        })
+        .then(data => console.log(data));
 }
 
 document.getElementById('btn-save').addEventListener('click', addAppartement);
 
 fetch('http://localhost:3000/users')
- .then(response => (response.json()))
- .then(data => {
-     data.forEach(element => {
-         console.log(element.firstName);
-         console.log(element.countryName)
-     });
- });
+    .then(response => (response.json()))
+    .then(data => {
+        data.forEach(element => {
+            console.log(element.firstName);
+            console.log(element.countryName)
+        });
+    });
 
 /*fetch('http://localhost:3000/users', {
     method: 'DELETE',
